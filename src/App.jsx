974425cc4146578119c48f4171d79d474c90df79
Sanowar72 <atifreza72ar@gmail.com>
2023-10-28 00:31:56 +0530

    axios usestate in detail and active link using usestate completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,71 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import styles from "./app.module.css";
+import Home from "./component/home/Home";
+import About from "./component/about/About";
+import Career from "./component/career/Career";
+import Reviews from "./component/reviews/Reviews";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [activeLink, setActiveLink] = useState("home");
+  const [data, setData] = useState([]);
 
-  function decrement() {
-    setCount(count - 1);
-  }
+  const getApiData = async () => {
+    try {
+      const myData = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setData(myData.data.slice(0, 4));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  useEffect(() => {
+    getApiData();
+  }, []);
+  
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>plus</button>
-      <button onClick={decrement}>minus</button>
+      <div className={styles.flex}>
+        <h4
+          onClick={() => setActiveLink("home")}
+          className={
+            activeLink === "home" ? styles.activeLink : styles.nonActiveLink
+          }
+        >
+          Home
+        </h4>
+        <h4
+          onClick={() => setActiveLink("about")}
+          className={
+            activeLink === "about" ? styles.activeLink : styles.nonActiveLink
+          }
+        >
+          About
+        </h4>
+        <h4
+          onClick={() => setActiveLink("career")}
+          className={
+            activeLink === "career" ? styles.activeLink : styles.nonActiveLink
+          }
+        >
+          Career
+        </h4>
+        <h4
+          onClick={() => setActiveLink("reviews")}
+          className={
+            activeLink === "reviews" ? styles.activeLink : styles.nonActiveLink
+          }
+        >
+          Reviews
+        </h4>
+      </div>
+      {activeLink === "home" && <Home userData={data} myname="sonu" />}
+      {activeLink === "about" && <About />}
+      {activeLink === "career" && <Career />}
+      {activeLink === "reviews" && <Reviews />}
     </>
   );
 }
