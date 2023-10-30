28e67f26c726b8bc1f6599ec7f222b1154ef8219
Sanowar72 <atifreza72ar@gmail.com>
2023-10-31 00:04:05 +0530

    added dummy data

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import data from "./dummy";
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const [MyRollNo, setMyRollNo] = useState("");
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const [age, setAge] = useState();
+  const [address, setAddress] = useState("");
+  const [rollNo, setRollNo] = useState();
 
-export default About
+  const getData = (e) => {
+    setMyRollNo(e.target.value);
+  };
+
+  const getParticulatStudentData = () => {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].rollNo == MyRollNo) {
+        return data[i];
+      }
+    }
+    return null;
+  };
+
+  const getUserData = () => {
+    if (MyRollNo) {
+      const stuData = getParticulatStudentData();
+      setName(stuData.name);
+      setGender(stuData.gender);
+      setAge(stuData.age);
+      const stuAddress = stuData?.address || "no Address";
+      setAddress(stuAddress);
+      setRollNo(stuData.rollNo);
+    }
+  };
+
+  return (
+    <>
+      <h1>ur roll no is {MyRollNo} </h1>
+      <input
+        type="number"
+        placeholder="please enter roll no"
+        onChange={getData}
+        value={MyRollNo}
+      />
+      <button onClick={getUserData} type="button">
+        search
+      </button>
+      <button onClick={getData}>reset roll no</button>
+      <div>
+        <h1>my student data</h1>
+
+        <h3>{name}</h3>
+        <h3>{rollNo}</h3>
+        <h3>{gender}</h3>
+        <h3>{age}</h3>
+        <h3>{address}</h3>
+      </div>
+    </>
+  );
+};
+
+export default About;
