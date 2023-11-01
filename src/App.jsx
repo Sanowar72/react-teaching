342f09d355c3137d278212f83bf5a7efdad79097
Sanowar72 <atifreza72ar@gmail.com>
2023-11-01 14:22:38 +0530

    useeffect in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,31 @@
-import { useEffect, useState } from "react";
-import React from "react";
-import styles from "./app.module.css";
-import Home from "./component/home/Home";
-import About from "./component/about/About";
-import Career from "./component/career/Career";
-import Reviews from "./component/reviews/Reviews";
-import axios from "axios";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 
-function App() {
-  const [activeLink, setActiveLink] = useState("home");
-  const [data, setData] = useState([]);
-
-  const getApiData = async () => {
-    try {
-      const myData = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      setData(myData.data.slice(0, 4));
-    } catch (error) {
-      console.log(error);
-    }
-  };
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [anotherCount, setAnontherCount] = useState(10);
 
   useEffect(() => {
-    getApiData();
-  }, []);
-  
+    console.log("component did mount");
+  }, [count, anotherCount]);
+
+  useEffect(() => {
+    console.log("second useeffect");
+  }, [count, anotherCount]);
+
+  useLayoutEffect(() => {
+    console.log("use layout effect");
+  }, [count, anotherCount]);
+
   return (
     <>
-      <div className={styles.flex}>
-        <h4
-          onClick={() => setActiveLink("home")}
-          className={
-            activeLink === "home" ? styles.activeLink : styles.nonActiveLink
-          }
-        >
-          Home
-        </h4>
-        <h4
-          onClick={() => setActiveLink("about")}
-          className={
-            activeLink === "about" ? styles.activeLink : styles.nonActiveLink
-          }
-        >
-          About
-        </h4>
-        <h4
-          onClick={() => setActiveLink("career")}
-          className={
-            activeLink === "career" ? styles.activeLink : styles.nonActiveLink
-          }
-        >
-          Career
-        </h4>
-        <h4
-          onClick={() => setActiveLink("reviews")}
-          className={
-            activeLink === "reviews" ? styles.activeLink : styles.nonActiveLink
-          }
-        >
-          Reviews
-        </h4>
-      </div>
-      {activeLink === "home" && <Home userData={data} myname="sonu" />}
-      {activeLink === "about" && <About />}
-      {activeLink === "career" && <Career />}
-      {activeLink === "reviews" && <Reviews />}
+      <h1>first count {count}</h1>
+      <h1>second count {anotherCount}</h1>
+      <button onClick={() => setCount(count + 1)}>click me for 1st cout</button>
+      <button onClick={() => setAnontherCount(anotherCount + 1)}>
+        second count
+      </button>
     </>
   );
-}
+};
 
 export default App;
