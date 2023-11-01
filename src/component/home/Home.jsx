9645729fee263b2b8805381105be9f2b4030d950
Sanowar72 <atifreza72ar@gmail.com>
2023-11-01 16:10:01 +0530

    form in progress using multiple states

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,22 +1,68 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const naviagate = useNavigate();
-  const myFunc = () => {
-    console.log("this is my func");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const resetFunc = () => {
+    setEmail("");
+    setName("");
+    setPassword("");
   };
-  useEffect(() => {
-    const myinterval = setInterval(myFunc, 1000);
-    return () => {
-      clearInterval(myinterval);
+  
+  const submitData = (e) => {
+    e.preventDefault();
+    const payload = {
+      name,
+      myemail: email,
+      password: password,
     };
-  }, []);
+    console.log(payload);
+  };
 
   return (
     <>
-      <div> this is Home</div>
-      <button onClick={() => naviagate("/about")}>go about</button>
+      <form onSubmit={submitData}>
+        <h1>Form in progress....</h1>
+        <h3>Please enter ur name</h3>
+        <input
+          type="text"
+          placeholder="enter ur name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          required
+        />
+        <h3>enter ur email</h3>
+        <input
+          type="email"
+          placeholder="enter ur email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+        <h3>ur password</h3>
+        <input
+          type={!showPassword ? "password" : "text"}
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+        <button type="button" onClick={() => setShowPassword(!showPassword)}>
+          {!showPassword ? "Show" : "Hide"}
+        </button>
+
+        <button type="submit" onSubmit={submitData}>
+          submit details
+        </button>
+      </form>
+      <button type="button" onClick={resetFunc}>
+        reset all value
+      </button>
+      <h1>student getails: {` ${name} ${email} ${password}`}</h1>
     </>
   );
 };
